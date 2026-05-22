d45f20bdf529a4875c0a5d989e2b9d2757fc6aed
Dmitry <dimyvybornov@gmail.com>
2026-05-22 21:00:09 +0300

    update

--- backup_service/settings.py
+++ backup_service/settings.py
@@ -134,6 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = 'backups/dashboard/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
